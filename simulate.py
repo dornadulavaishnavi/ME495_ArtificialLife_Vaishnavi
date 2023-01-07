b8ab817a718f0ebb71a11ed125bbd50a9a4883a9
vdornadula <vaishnavidornadula2026@u.northwestern.edu>
2023-01-07 14:43:16 -0600

    opens simulation for longer time with sleep

--- simulate.py
+++ simulate.py
@@ -1,8 +1,11 @@
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
 
-for i in range(100):
+for i in range(1000):
     p.stepSimulation()
-    
+    time.sleep((1/600))
+    print(i)
+
 p.disconnect() 
