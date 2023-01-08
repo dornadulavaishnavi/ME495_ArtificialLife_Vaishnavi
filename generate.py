3d906bc775f7571121a0e9c65ae292685ec3430f
vdornadula <vaishnavidornadula2026@u.northwestern.edu>
2023-01-08 00:03:32 -0600

    working on front leg

--- generate.py
+++ generate.py
@@ -18,10 +18,10 @@ def Create_World():
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name="Torso", pos=[1.5,0,1.5] , size=[1.0,1.0,1.0])
-    pyrosim.Send_Joint( name = "Torso_Back_Leg" , parent= "Torso" , child = "Back_Leg" , type = "revolute", position = [1.0,0.0,1.0])
-    pyrosim.Send_Cube(name="Back_Leg", pos=[-0.5,0.0,-0.5] , size=[1,1,1])
-    # pyrosim.Send_Joint( name = "Torso_Front_Leg" , parent= "Torso" , child = "Front_Leg" , type = "revolute", position = [0.5,0.0,-0.5])
-    # pyrosim.Send_Cube(name="Front_Leg", pos=[0.5,0.0,-0.5] , size=[1,1,1])
+    pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [1.0,0.0,1.0])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0.0,-0.5] , size=[1,1,1])
+    pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [1.0,0.0,0.0])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0.0,-0.5] , size=[1,1,1])
     pyrosim.End()
 
 def Create_Robot_Example():
