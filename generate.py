4315c8551add6d6e5e5a4fcfc25e57510be01579
vdornadula <vaishnavidornadula2026@u.northwestern.edu>
2023-01-07 21:24:05 -0600

    progress in links

--- generate.py
+++ generate.py
@@ -1,15 +1,26 @@
 import pyrosim.pyrosim as pyrosim
 
-length = 1
-width = 1
-height = 1
+def Create_World():
+    length = 1
+    width = 1
+    height = 1
 
-x = 0
-y = 0
-z = height/2
+    x = 0
+    y = 0
+    z = height/2
 
-pyrosim.Start_SDF("world.sdf")
+    pyrosim.Start_SDF("world.sdf")
 
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+    pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
 
-pyrosim.End()
+    pyrosim.End()
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[0,0,0.5] , size=[1.0,1.0,1.0])
+    pyrosim.Send_Joint( name = "Torso_Leg" , parent= "Torso" , child = "Leg" , type = "revolute", position = [0.5,0,1.0])
+    pyrosim.Send_Cube(name="Leg", pos=[1.0,0,1.5] , size=[1,1,1])
+    pyrosim.End()
+
+Create_World()
+Create_Robot()
