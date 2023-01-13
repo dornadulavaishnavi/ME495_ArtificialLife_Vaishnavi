970d5600f788a5ff688fd96e907245f1ba47b8e0
vdornadula <vaishnavidornadula2026@u.northwestern.edu>
2023-01-13 17:38:04 -0600

    plots sensor data

--- analyze.py
+++ analyze.py
@@ -1,1 +1,8 @@
 import numpy
+import matplotlib.pyplot
+
+sensor_data = numpy.load("data/backLegSensordata.npy")
+# print(sensor_data)
+
+matplotlib.pyplot.plot(sensor_data)
+matplotlib.pyplot.show()
