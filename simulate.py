b0d93346da6901822836d20ba1d3ff714dc11338
vdornadula <vaishnavidornadula2026@u.northwestern.edu>
2023-01-14 13:56:24 -0600

    motors in simulate no longer throws error

--- simulate.py
+++ simulate.py
@@ -22,6 +22,7 @@ for i in range(simlength):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName = b'Torso_BackLeg', controlMode = p.POSITION_CONTROL, targetPosition = 0.0, maxForce = 500)
     time.sleep((1/200))
     # print(i)
 
