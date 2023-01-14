81dd416374a16b0b359d00ecb00879d773bf71ef
vdornadula <vaishnavidornadula2026@u.northwestern.edu>
2023-01-14 12:20:05 -0600

    plots with all properties

--- analyze.py
+++ analyze.py
@@ -5,7 +5,7 @@ backLegSensor_data = numpy.load("data/backLegSensordata.npy")
 frontLegSensor_data = numpy.load("data/frontLegSensordata.npy")
 # print(sensor_data)
 
-matplotlib.pyplot.plot(backLegSensor_data, label = 'Back Leg Sensor Data')
+matplotlib.pyplot.plot(backLegSensor_data, label = 'Back Leg Sensor Data', linewidth = 3)
 matplotlib.pyplot.plot(frontLegSensor_data, label = "Front Leg Sensor Data")
 
 matplotlib.pyplot.legend()
