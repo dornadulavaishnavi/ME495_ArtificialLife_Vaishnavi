f68f989c40af841fed7f30d8baa2557a272feda0
vdornadula <vaishnavidornadula2026@u.northwestern.edu>
2023-01-15 09:18:36 -0600

    simulation opens empty world

--- robot.py
+++ robot.py
@@ -1,5 +1,6 @@
 class ROBOT:
 
     def __init__(self):
-
-        pass
+        self.sensors = {}
+        self.motors = {}
+        
--- simulation.py
+++ simulation.py
@@ -1,3 +1,6 @@
+import pybullet as p
+import pybullet_data
+import pyrosim.pyrosim as pyrosim
 from world import WORLD
 from robot import ROBOT
 
@@ -7,3 +10,10 @@ class SIMULATION:
 
         self.world = WORLD()
         self.robot = ROBOT()
+        self.physicsClient = p.connect(p.GUI)
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+        p.setGravity(0,0,-9.8)
+        self.robotId = p.loadURDF("body.urdf")
+
+        pyrosim.Prepare_To_Simulate(self.robotId)
