0692013e537dd9c24ad2d181bd36341e56229431
vdornadula <vaishnavidornadula2026@u.northwestern.edu>
2023-01-21 14:13:46 -0600

    motor neuron added

--- pyrosim/neuralNetwork.py
+++ pyrosim/neuralNetwork.py
@@ -29,7 +29,9 @@ class NEURAL_NETWORK:
         print("")
 
     def Update(self):
-        pass
+        for neuronName in self.neurons:
+            if self.neurons[neuronName].Is_Sensor_Neuron():
+                self.neurons[neuronName].Update_Sensor_Neuron()
 
 # ---------------- Private methods --------------------------------------
 
