490d9bc7ea2a4de8393f52d1c5f41ac2da743600
vdornadula <vaishnavidornadula2026@u.northwestern.edu>
2023-01-24 22:28:21 -0600

    created hill climber class

--- search.py
+++ search.py
@@ -1,5 +1,7 @@
 import os
+from hillclimber import HILL_CLIMBER
 
-for run in range(5):
-    os.system("python generate.py")
-    os.system("python simulate.py")
+hc = HILL_CLIMBER()
+# for run in range(5):
+#     os.system("python generate.py")
+#     os.system("python simulate.py")
