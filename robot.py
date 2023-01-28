b258965861c2d4f29ea34a6c8af2d56c5eda606d
vdornadula <vaishnavidornadula2026@u.northwestern.edu>
2023-01-28 15:05:16 -0600

    saves x coordinate into fitness file

--- robot.py
+++ robot.py
@@ -55,5 +55,8 @@ class ROBOT:
         stateOfLinkZero = p.getLinkState(self.robotId,0)
         positionOfLinkZero = stateOfLinkZero[0]
         xCoordinateOfLinkZero = positionOfLinkZero[0]
-        print(positionOfLinkZero)
+        print(xCoordinateOfLinkZero)
+        f = (open("fitness.txt", "w"))
+        f.write(str(xCoordinateOfLinkZero))
+        f.close()
         exit()
