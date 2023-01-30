4388f96e138ee45f7f51f534675475b6deadbb32
vdornadula <vaishnavidornadula2026@u.northwestern.edu>
2023-01-30 15:25:24 -0600

    fixed quadruped's leg positions, had the wrong parent

--- solution.py
+++ solution.py
@@ -85,13 +85,13 @@ class SOLUTION:
         pyrosim.Send_Joint( name = "Torso_RightLeg" , parent= "Torso" , child = "RightLeg" , type = "revolute", position = [0.5,0.0,1.0], jointAxis = "1 0 0")
         pyrosim.Send_Cube(name="RightLeg", pos=[0.5,0.0,0.0] , size=[1,0.2,0.2])
 
-        pyrosim.Send_Joint( name = "Torso_BackLowerLeg" , parent= "Torso" , child = "BackLowerLeg" , type = "revolute", position = [0.0,-1.0,0.0], jointAxis = "1 0 0")
+        pyrosim.Send_Joint( name = "Torso_BackLowerLeg" , parent= "BackLeg" , child = "BackLowerLeg" , type = "revolute", position = [0.0,-1.0,0.0], jointAxis = "1 0 0")
         pyrosim.Send_Cube(name="BackLowerLeg", pos=[0.0,0.0,-0.5] , size=[0.2,0.2,1])
-        pyrosim.Send_Joint( name = "Torso_FrontLowerLeg" , parent= "Torso" , child = "FrontLowerLeg" , type = "revolute", position = [0.0,1.0,0.0], jointAxis = "1 0 0")
+        pyrosim.Send_Joint( name = "Torso_FrontLowerLeg" , parent= "FrontLeg" , child = "FrontLowerLeg" , type = "revolute", position = [0.0,1.0,0.0], jointAxis = "1 0 0")
         pyrosim.Send_Cube(name="FrontLowerLeg", pos=[0.0,0.0,-0.5] , size=[0.2,0.2,1])
-        pyrosim.Send_Joint( name = "Torso_LeftLowerLeg" , parent= "Torso" , child = "LeftLowerLeg" , type = "revolute", position = [-1.0,0.0,0.0], jointAxis = "1 0 0")
+        pyrosim.Send_Joint( name = "Torso_LeftLowerLeg" , parent= "LeftLeg" , child = "LeftLowerLeg" , type = "revolute", position = [-1.0,0.0,0.0], jointAxis = "1 0 0")
         pyrosim.Send_Cube(name="LeftLowerLeg", pos=[0.0,0.0,-0.5] , size=[0.2,0.2,1])
-        pyrosim.Send_Joint( name = "Torso_RightLowerLeg" , parent= "Torso" , child = "RightLowerLeg" , type = "revolute", position = [1.0,0.0,0.0], jointAxis = "1 0 0")
+        pyrosim.Send_Joint( name = "Torso_RightLowerLeg" , parent= "RightLeg" , child = "RightLowerLeg" , type = "revolute", position = [1.0,0.0,0.0], jointAxis = "1 0 0")
         pyrosim.Send_Cube(name="RightLowerLeg", pos=[0.0,0.0,-0.5] , size=[0.2,0.2,1])
 
         pyrosim.End()
