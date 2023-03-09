324e572900a1659af13e08f53cf97bbf76b620cd
vdornadula <vaishnavidornadula2026@u.northwestern.edu>
2023-03-08 19:44:58 -0600

    with 3 bodies

--- solution.py
+++ solution.py
@@ -17,7 +17,7 @@ class SOLUTION:
         # print(self.weight)
         random.seed = 5
         self.weight = self.weight*2-1
-        self.numLinks = 2 #random.randint(1,6)
+        self.numLinks = 3 #random.randint(1,6)
         self.num_leg_extentions = 5 # random.randint(0,5)
         self.block_size = 0.75
         self.vert_cube_bound = [self.block_size/4,self.block_size/4,self.block_size/4] # random.uniform(0,self.block_size)
